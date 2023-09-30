34098d803d32d527006d797dcff6a65549e52db1
Robin Nook <ronook@ttu.ee>
2023-09-30 16:23:21 +0300

    All PUT endpoints and DELETE endpoints implemented

--- TalDate/src/main/java/com/taldate/backend/user/controller/UserController.java
+++ TalDate/src/main/java/com/taldate/backend/user/controller/UserController.java
@@ -1,13 +1,36 @@
 package com.taldate.backend.user.controller;
 
+import com.taldate.backend.user.dto.UserDTO;
 import com.taldate.backend.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
 
+    @PutMapping("/user/password/{id}")
+    public UserDTO updatePassword(@PathVariable Integer id, @RequestBody UserDTO userDTO) {
+        return userService.updatePassword(id, userDTO);
+    }
+
+    @PutMapping("/user/email/{id}")
+    public UserDTO updateEmail(@PathVariable Integer id, @RequestBody UserDTO userDTO) {
+        return userService.updateEmail(id, userDTO);
+    }
+
+    @PutMapping("/user/name/{id}")
+    public UserDTO updateName(@PathVariable Integer id, @RequestBody UserDTO userDTO) {
+        return userService.updateName(id, userDTO);
+    }
+
+    @DeleteMapping("/user/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Integer id) {
+        userService.deleteUserByID(id);
+        return ResponseEntity.noContent().build();
+    }
 
 }
--- TalDate/src/main/java/com/taldate/backend/user/service/UserService.java
+++ TalDate/src/main/java/com/taldate/backend/user/service/UserService.java
@@ -1,11 +1,44 @@
 package com.taldate.backend.user.service;
 
+import com.taldate.backend.user.dto.UserDTO;
+import com.taldate.backend.user.entity.User;
 import com.taldate.backend.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+
+    public UserDTO updatePassword(Integer id, UserDTO userDTO) {
+            User user = userRepository.findById(id)
+                    .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found"));
+            user.setPassword_hash(userDTO.password_hash());
+            userRepository.save(user);
+            return userDTO;
+    }
+
+    public UserDTO updateEmail(Integer id, UserDTO userDTO) {
+            User user = userRepository.findById(id)
+                    .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found"));
+            user.setEmail(userDTO.email());
+            userRepository.save(user);
+            return userDTO;
+    }
+
+    public UserDTO updateName(Integer id, UserDTO userDTO) {
+            User user = userRepository.findById(id)
+                    .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found"));
+            user.setFirstName(userDTO.firstName());
+            user.setLastName(userDTO.lastName());
+            userRepository.save(user);
+            return userDTO;
+    }
+
+    public void deleteUserByID(Integer id) {
+        userRepository.deleteById(id);
+    }
 }
