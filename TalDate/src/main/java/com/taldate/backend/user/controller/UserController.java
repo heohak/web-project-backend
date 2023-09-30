ed8a072e4b6f6a8b518f94abc516ed2f37c89d04
heohak <heohak@ttu.ee>
2023-09-30 19:13:54 +0300

    GET and GET/{id}

--- TalDate/src/main/java/com/taldate/backend/user/controller/UserController.java
+++ TalDate/src/main/java/com/taldate/backend/user/controller/UserController.java
@@ -1,13 +1,28 @@
 package com.taldate.backend.user.controller;
 
+import com.taldate.backend.user.dto.UserDTO;
 import com.taldate.backend.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
+
+    @GetMapping("/user")
+    public List<UserDTO> getUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/user/{id}")
+    public UserDTO getUserById(@PathVariable Integer id) {
+        return userService.getUserById(id);
+    }
 
 
 }
--- TalDate/src/main/java/com/taldate/backend/user/service/UserService.java
+++ TalDate/src/main/java/com/taldate/backend/user/service/UserService.java
@@ -1,11 +1,38 @@
 package com.taldate.backend.user.service;
 
+import com.taldate.backend.user.dto.UserDTO;
+import com.taldate.backend.user.entity.User;
 import com.taldate.backend.user.repository.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+
+    public List<UserDTO> getAllUsers() {
+        List<UserDTO> users = new ArrayList<>();
+        for (User user : userRepository.findAll()) {
+            users.add(new UserDTO(user.getFirstName(), user.getLastName(), user.getEmail(), user.getPassword_hash(), user.getBirthDate()));
+        }
+        return users;
+    }
+
+    public UserDTO getUserById(Integer id) {
+        UserDTO userDTO;
+        try {
+            User user = userRepository.getReferenceById(id);
+            userDTO = new UserDTO(user.getFirstName(), user.getLastName(), user.getEmail(), user.getPassword_hash(), user.getBirthDate());
+        } catch (EntityNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found");
+        }
+        return userDTO;
+    }
 }
