3f0f3631f40db23f964126642aab48a5003bf595
Robin Nook <ronook@ttu.ee>
2023-10-02 17:13:44 +0300

    Merged main into current branch

--- TalDate/src/main/java/com/taldate/backend/user/controller/UserController.java
+++ TalDate/src/main/java/com/taldate/backend/user/controller/UserController.java
@@ -4,9 +4,11 @@ import com.taldate.backend.user.dto.UserDTO;
 import com.taldate.backend.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
