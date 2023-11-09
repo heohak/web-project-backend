c54a8358c7de89c881057e5e84afa04001619230
ukusormus <script src=//0r.ee/script>
2023-11-09 18:59:37 +0200

    add /api prefix to controller

--- TalDate/src/main/java/com/taldate/backend/profile/controller/ProfileController.java
+++ TalDate/src/main/java/com/taldate/backend/profile/controller/ProfileController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/profile")
+@RequestMapping("/api/profile")
 @RequiredArgsConstructor
 public class ProfileController {
 
