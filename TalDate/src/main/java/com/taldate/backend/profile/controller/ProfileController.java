9f1d27a8d2413846726ffb5b0e1c1a48110c178b
ukusormus <script src=//0r.ee/script>
2023-12-07 19:54:21 +0200

    differ endpoints

--- TalDate/src/main/java/com/taldate/backend/profile/controller/ProfileController.java
+++ TalDate/src/main/java/com/taldate/backend/profile/controller/ProfileController.java
@@ -1,6 +1,9 @@
 package com.taldate.backend.profile.controller;
 
 import com.taldate.backend.profile.dto.ProfileDTO;
+import com.taldate.backend.profile.dto.UpdateBioDTO;
+import com.taldate.backend.profile.dto.UpdateGenderPreferenceDTO;
+import com.taldate.backend.profile.dto.UpdateProfilePictureDTO;
 import com.taldate.backend.profile.service.ProfileService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -18,8 +21,18 @@ public class ProfileController {
     }
 
     @PutMapping
-    public void updateProfile(@RequestBody ProfileDTO dto) {
-        profileService.updateProfile(dto);
+    public void updateGenderPreference(@RequestBody UpdateGenderPreferenceDTO dto) {
+        profileService.updateGenderPreference(dto);
+    }
+
+    @PutMapping
+    public void updateBio(@RequestBody UpdateBioDTO dto) {
+        profileService.updateBio(dto);
+    }
+
+    @PutMapping
+    public void updateProfilePicture(@RequestBody UpdateProfilePictureDTO dto) {
+        profileService.updateProfilePicture(dto);
     }
 
     @GetMapping("/random")
---
+++ TalDate/src/main/java/com/taldate/backend/profile/dto/UpdateProfilePictureDTO.java
@@ -0,0 +1,4 @@
+package com.taldate.backend.profile.dto;
+
+public record UpdateProfilePictureDTO(String profilePicture) {
+}
