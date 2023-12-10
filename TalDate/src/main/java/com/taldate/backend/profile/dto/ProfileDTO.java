ddd312ca615c80ded69c03130862cb4f712cb905
heohak <heohak@ttu.ee>
2023-12-10 21:46:39 +0200

    fix

--- TalDate/src/main/java/com/taldate/backend/profile/dto/ProfileDTO.java
+++ TalDate/src/main/java/com/taldate/backend/profile/dto/ProfileDTO.java
@@ -1,12 +1,10 @@
 package com.taldate.backend.profile.dto;
 
-import com.taldate.backend.picture.Picture;
-
 public record ProfileDTO(String name,
                          int age,
                          boolean genderPreferenceMale,
                          String bio,
-                         Picture picture,
+                         String picture,
                          boolean profileActive,
                          boolean genderMale) {
 }
